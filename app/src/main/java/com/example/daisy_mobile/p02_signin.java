4d9ce4d1cbb8aa109e6af81a7f3e523900115f7d
Tú Văn Lục <19521097@gm.uit.edu.vn>
2021-10-29 04:27:59 +0700

    page p02 p03 p05 p04(home_fragement :search bar) ui

--- app/src/main/java/com/example/daisy_mobile/p02_signin.java
+++ app/src/main/java/com/example/daisy_mobile/p02_signin.java
@@ -2,7 +2,12 @@ package com.example.daisy_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class p02_signin extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class p02_signin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p02_signin);
+
+                TextView next = (TextView) findViewById(R.id.et_p02_register);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                next.setTextColor(Color.parseColor("#FFFFFF"));
+                startActivity(new Intent(p02_signin.this, p03_signup.class));
+
+            }
+
+        });
+
+        Button signin = (Button) findViewById(R.id.bt_p02_signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                startActivity(new Intent(p02_signin.this, MainActivity.class));
+
+            }
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/daisy_mobile/p05_search.java
@@ -0,0 +1,14 @@
+package com.example.daisy_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class p05_search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p05_search);
+    }
+}
