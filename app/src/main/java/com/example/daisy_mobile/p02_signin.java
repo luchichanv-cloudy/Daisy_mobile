301d32654c0caa3769e95cb9e9c425aae1a980d1
Tú Văn Lục <19521097@gm.uit.edu.vn>
2021-11-04 02:42:46 +0700

    page 03 da signup duoc bang authentication voi email

--- app/src/main/java/com/example/daisy_mobile/p02_signin.java
+++ app/src/main/java/com/example/daisy_mobile/p02_signin.java
@@ -1,13 +1,26 @@
 package com.example.daisy_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.AsyncListUtil;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 public class p02_signin extends AppCompatActivity {
 
@@ -16,7 +29,13 @@ public class p02_signin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p02_signin);
 
-                TextView next = (TextView) findViewById(R.id.et_p02_register);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+
+
+        TextView next = (TextView) findViewById(R.id.et_p02_register);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 next.setTextColor(Color.parseColor("#FFFFFF"));
@@ -26,11 +45,10 @@ public class p02_signin extends AppCompatActivity {
 
         });
 
-        Button signin = (Button) findViewById(R.id.bt_p02_signin);
+        Button signin = (Button) findViewById(R.id.p02_bt_signin);
         signin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
-                startActivity(new Intent(p02_signin.this, MainActivity.class));
 
             }
 
--- app/src/main/java/com/example/daisy_mobile/p03_signup.java
+++ app/src/main/java/com/example/daisy_mobile/p03_signup.java
@@ -1,8 +1,21 @@
 package com.example.daisy_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class p03_signup extends AppCompatActivity {
 
@@ -10,5 +23,59 @@ public class p03_signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p03_signup);
+
+       FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.p03_progressBar);
+        Button btnSignUp = (Button) findViewById(R.id.p03_btn_signup);
+        EditText inputEmail = (EditText) findViewById(R.id.p03_et_email);
+        EditText inputPassword = (EditText) findViewById(R.id.p03_et_password);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(p03_signup.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(p03_signup.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(p03_signup.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(p03_signup.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
+
 }
