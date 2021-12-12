2ea4d75fa35b16cdd6ba9433a25724465278ac4f
your name <19520856@gm.uit.edu.vn>
2021-12-12 17:32:40 +0700

    giao dien tam thoi p04, p06, p07, p16

---
+++ app/src/main/java/com/example/daisy_mobile/p16_favorite.java
@@ -0,0 +1,14 @@
+package com.example.daisy_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class p16_favorite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p16_favorite);
+    }
+}
