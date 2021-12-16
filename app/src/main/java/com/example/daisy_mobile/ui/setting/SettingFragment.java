1c861923c91fbe45151e93260e0e627089451d05
Tú Văn Lục <19521097@gm.uit.edu.vn>
2021-12-17 02:59:52 +0700

    - Add image slider to home fragment( not finished)

--- app/src/main/java/com/example/daisy_mobile/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/daisy_mobile/ui/setting/SettingFragment.java
@@ -29,13 +29,7 @@ public class SettingFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        settingViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 
