f936e260e314e252bef47723845541de983c6211
your name <19520856@gm.uit.edu.vn>
2022-01-02 10:56:09 +0700

    favourite kitchen

---
+++ app/src/main/java/dataclass/kitchen_favourite.java
@@ -0,0 +1,27 @@
+package dataclass;
+
+public class kitchen_favourite {
+    private String kitchenid, userid;
+    public kitchen_favourite(String kitchenid, String userid){
+        this.kitchenid=kitchenid;
+        this.userid=userid;
+    }
+    public kitchen_favourite(){}
+    public void setKitchenid(String kitchenid) {
+        this.kitchenid = kitchenid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+
+
+    public String getKitchenid() {
+        return kitchenid;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+}
