d8438929c42baec5f12bcf432a9a68530258eb06
Van Luc-Tu <luctuvan2001@gmail.cm>
2022-01-14 22:46:29 +0700

    - Finish kitchenside sale fragment

--- app/src/main/java/com/example/daisy_mobile/adapter/KitchensalesItemAdapter.java
+++ app/src/main/java/com/example/daisy_mobile/adapter/KitchensalesItemAdapter.java
@@ -1,19 +1,37 @@
 package com.example.daisy_mobile.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
+
+import com.bumptech.glide.Glide;
 import com.example.daisy_mobile.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 import dataclass.item;
+import dataclass.order;
 
 public class KitchensalesItemAdapter extends ArrayAdapter<item>  {
 
@@ -33,9 +51,54 @@ public class KitchensalesItemAdapter extends ArrayAdapter<item>  {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_kitchensales, parent, false);
         }
-        ToggleButton tog = (ToggleButton) convertView.findViewById(R.id.toggleButton);
+        Switch s=(Switch)convertView.findViewById(R.id.toggleButton);
+
         ImageView img=(ImageView) convertView.findViewById(R.id.img);
+        TextView name=(TextView)convertView.findViewById(R.id.tv_name);
+        name.setText(item.getName());
+        getimage(item.getImageID(),img);
+        if (item.isSalestatus())
+        {
+            s.setChecked(true);
+        }
+        else {
+            s.setChecked(false);
+        }
+        s.setTag(position);
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update(s.isChecked(),position);
+            }
+        });
 
         return convertView;
+    }
+    void update(boolean value,int position)
+    {
+
+        item a=items.get(position);
+        a.setSalestatus(value);
+        notifyDataSetChanged();
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+       db.collection("item").document(a.getImageID()).update("salestatus",a.isSalestatus());
+    }
+    private void getimage(String url, ImageView displayview)
+    {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        storageRef.child("images/"+url).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Got the download URL for 'users/me/profile.png'
+                Glide.with(getContext() /* context */)
+                        .load(uri)
+                        .into(displayview);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+            }
+        });
     }
 }
--- app/src/main/java/com/example/daisy_mobile/ui/sales/Kitchensales.java
+++ app/src/main/java/com/example/daisy_mobile/ui/sales/Kitchensales.java
@@ -1,25 +1,70 @@
 package com.example.daisy_mobile.ui.sales;
 
+import static com.example.daisy_mobile.p02_signin.user_id;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.daisy_mobile.R;
+import com.example.daisy_mobile.adapter.FoodItemAdapter;
+import com.example.daisy_mobile.adapter.KitchensalesItemAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import dataclass.item;
 
 
 public class Kitchensales extends Fragment {
-
-
+    private ListView lv;
+    private ArrayList<item> items;
+    private KitchensalesItemAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_kitchensales, container, false);
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        String user_id= FirebaseAuth.getInstance().getUid();
+        lv = (ListView) view.findViewById(R.id.lv_sale);
 
+        db = FirebaseFirestore.getInstance();
+        db.collection("item")
+                .whereEqualTo("kitchenID",user_id)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        //    item ia=new item("denden","ae422c35-f366-40a7-bbee-67a82979d7b2","hello","drink","3vWBk5eX7lcjexVK0zVXLYPtFFq1",50,30,false);
+                        items = new ArrayList<item>();
+                        adapter = new KitchensalesItemAdapter(getContext(), items);
+                        lv.setAdapter(adapter);
+                        //   arrayOfitem.add(ia);
+                        if (task.isSuccessful()) {
+                            int i=0;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                item abc=document.toObject(item.class);
+                                items.add(abc);adapter.notifyDataSetChanged();
+                            }
+
+                        } else {
+                            //   Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
         return view;
     }
 }
